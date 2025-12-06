641e1824e704a1e94a2a037f0076b2e910bdfe63
komaljadhav116 <komal.v.jadhav2@gmail.com>
2025-12-06 11:07:03 +0530

    add changes

--- app/server.js
+++ app/server.js
@@ -14,6 +14,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
